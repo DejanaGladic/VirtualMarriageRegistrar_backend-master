23aa7fb6a6a855f8c77c1c9397d98d952fc3b0b4
DejanaGladic <dejanagladic@uns.ac.rs>
2023-01-20 14:04:58 +0100

    VirtualMarriageRegistrar-backend

---
+++ src/main/java/dj/ctrls/MuzRestController.java
@@ -0,0 +1,31 @@
+package dj.ctrls;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import dj.jpa.Muz;
+import dj.repository.MuzRepository;
+
+@CrossOrigin
+@RestController
+public class MuzRestController {
+
+	@Autowired
+	private MuzRepository muzRepository;
+
+	//Proba commit
+	// insert
+	@PostMapping("muz")
+	public Muz insertMuz(@RequestBody Muz muz) {
+		if (!muzRepository.existsById(muz.getId())) {
+
+			Muz muzVracen = muzRepository.save(muz);
+
+			return muzVracen;
+		}
+		return null;
+	}
+}
---
+++ src/main/java/dj/ctrls/TerminRestController.java
@@ -0,0 +1,46 @@
+package dj.ctrls;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import dj.jpa.Termin;
+import dj.repository.TerminRepository;
+
+
+@CrossOrigin
+@RestController
+public class TerminRestController {
+
+	@Autowired
+	private TerminRepository terminRepository;
+
+
+	// insert
+	@PostMapping("termin")
+	public Termin insertTermin(@RequestBody Termin termin) {
+		if (!terminRepository.existsById(termin.getId())) {
+
+			Termin terminVracen = terminRepository.save(termin);
+
+			return terminVracen;
+		}
+		return null;
+	}
+	
+	@PutMapping("termin")
+	public Termin updateTermin(@RequestBody Termin termin) {
+		if (terminRepository.existsById(termin.getId())) {
+
+			Termin terminVracen = terminRepository.save(termin);
+
+			return terminVracen;
+		}
+		return null;
+	}
+}
